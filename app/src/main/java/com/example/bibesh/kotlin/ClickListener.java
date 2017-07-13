f6cbd449f3ef28f432315d2bdc70ea0de55df34b
Bibesh Manandhar <bibesh.manandhar@gmail.com>
2017-07-13 13:25:56 +0545

    INITIAL COMMIT

---
+++ app/src/main/java/com/example/bibesh/kotlin/ClickListener.java
@@ -0,0 +1,9 @@
+package com.example.bibesh.kotlin;
+
+/**
+ * Created by Bibesh on 7/13/17.
+ */
+
+public interface ClickListener {
+    void onItemClick(int position);
+}
---
+++ app/src/main/java/com/example/bibesh/kotlin/PersonModel.java
@@ -0,0 +1,26 @@
+package com.example.bibesh.kotlin;
+
+/**
+ * Created by Bibesh on 7/13/17.
+ */
+
+public class PersonModel {
+    private String name;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
